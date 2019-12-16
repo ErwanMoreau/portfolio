ac9c9ec8051bf6c5adb35c5c11a2b6b31d567853
Erwan <tzeback@gmail.com>
2019-12-16 17:57:31 +0100

    add some content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsStoragePluginModule, StorageOption } from '@ngxs/storage-plugin';
 import { environment } from 'src/environments/environment';
+import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { ServiceComponent } from './service/service.component';
 import { LanguageState } from './shared/store/state/language.state';
 import { TopMenuComponent } from './top-menu/top-menu.component';
-import { HeaderComponent } from './header/header.component';
+import { CompetenceComponent } from './competence/competence.component';
+
+const appRoutes: Routes = [
+
+  {
+    path: '',
+    component: PrincipalComponent,
+    data: { title: 'Heroes List' }
+  }
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    PrincipalComponent,
+    ServiceComponent,
+    CompetenceComponent
   ],
   imports: [
     BrowserModule,
@@ -21,8 +43,12 @@ import { HeaderComponent } from './header/header.component';
     NgxsModule.forRoot([
       LanguageState
     ], { developmentMode: !environment.production }),
-    NgxsStoragePluginModule.forRoot({storage: StorageOption.LocalStorage})
+    NgxsStoragePluginModule.forRoot({storage: StorageOption.LocalStorage}),
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
